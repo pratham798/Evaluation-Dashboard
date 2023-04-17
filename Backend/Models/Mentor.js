8378101607ee48f6b495b465128e43c3fe5b0ea4
Pratham Jamnal <prathamjamnal7982@gmail.com>
2023-04-18 03:53:11 +0530

    Add Validation to Mentor Schema

--- Backend/Models/Mentor.js
+++ Backend/Models/Mentor.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const mentor = mongoose.Schema({
   MentorName: { type: String, required: true },
   MentorId: { type: String, required: true },
-  StudentCount: { type: Number, default: 0 },
+  StudentCount: { type: Number, min: 3, max: 4, default: 0 },
 });
 
 const MentorSchema = mongoose.model("Mentors", mentor);
